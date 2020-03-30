483ad9daf00c51f60eacc250d52b39f7d8969bd6
Akira Yumiyama <yumiyama.akira@gmail.com>
2020-03-30 15:49:26 +0900

    Bump v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 import os
 
 name = 'datastore-viewer'
-version = '0.2.0b2'
+version = '0.2.0'
 description = 'Datastore Viewer for emulator'
 dependencies = [
     'google-cloud-datastore >=1.7.0, >= 1.8.0',
