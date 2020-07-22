0340504c1fe18a407bcc81018fe3c934b33f92ad
MakTak <justaway22@gmail.com>
2020-07-22 21:09:50 +0900

    indexされていない要素は並べ替えできないようにする

--- webapp/src/pages/EntityList/internal/EntityListBody/EntityListBody.tsx
+++ webapp/src/pages/EntityList/internal/EntityListBody/EntityListBody.tsx
@@ -203,19 +203,7 @@ const EnhancedTableHead: React.FunctionComponent<EnhancedTableProps> = ({
               padding="default"
               sortDirection={orderBy === headCell.id ? order : false}
               style={{ fontWeight: 'bolder', color: 'grey' }}>
-              <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : 'asc'}
-                onClick={createSortHandler(headCell.id)}>
-                {headCell.label}
-                {orderBy === headCell.id && (
-                  <span className={classes.visuallyHidden}>
-                    {order === 'desc'
-                      ? 'sorted descending'
-                      : 'sorted ascending'}
-                  </span>
-                )}
-              </TableSortLabel>
+              {headCell.label}
             </TableCell>
           );
         })}
